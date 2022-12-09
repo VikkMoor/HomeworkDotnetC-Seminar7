c17a905d1cc45f9beff435e39f3f532bdaf103b0
Vikk_Moor <victimofuroboros@gmail.com>
2022-12-09 16:04:50 +0500

    1stReady

--- 1st/Program.cs
+++ 1st/Program.cs
@@ -1,3 +1,29 @@
-﻿
+﻿void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            int num1 = new Random().Next(-10, 11);
+            int num2 = new Random().Next(-10, 11);
+            int num3 = new Random().Next(-10, 11);
+            double num = (num1 + num2 + num3) / 3.0;
+            matr[i, j] = Math.Round(num, 1);
+        }
+    }
+}
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
 
-
+double[,] matrix = new double[3, 4];
+FillArray(matrix);
+PrintArray(matrix);
